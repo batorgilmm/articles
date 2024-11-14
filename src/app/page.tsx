1a5be963d8e183c07f8ac818e3eb172b6e189feb
batorgil Munkhzorig <orgil@batorgils-MacBook-Air.local>
2024-11-14 22:57:44 +0800

    add components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,27 @@
-// import fetch from 'node-fetch';
-// import LinkPreview from "@/components/LinkPreview";
+import fetch from 'node-fetch';
+import LinkPreview from "@/components/LinkPreview";
 
 
-// type ArticleType = {
-//   _id: string
-//   url: string
-//   createdAt: string
-//   updatedAt: string
-// }
+type ArticleType = {
+  _id: string
+  url: string
+  createdAt: string
+  updatedAt: string
+}
 
-// type ApiResponse = {
-//   response: ArticleType[];
-// }
+type ApiResponse = {
+  response: ArticleType[];
+}
 
 export default async function Home() {
 
-  // const data = await fetch(`${process.env.NEXT_PUBLIC_ORIGIN_URL}/api/article`)
+  const data = await fetch(`${process.env.NEXT_PUBLIC_ORIGIN_URL}/api/article`)
 
-  // console.log(data)
+  console.log(data)
 
-  // const { response: articles } = (await data.json()) as ApiResponse
+  const { response: articles } = (await data.json()) as ApiResponse
 
-  // console.log(articles)
+  console.log(articles)
   return (
     <div className="min-h-screen sm:p-20">
       <div className="max-w-[616px] mx-auto">
@@ -30,9 +30,9 @@ export default async function Home() {
         </h1>
 
         <div className=" h-full mt-10 relative flex justify-center">
-          {/* {articles.map((article) => (
+          {articles.map((article) => (
             <LinkPreview key={article._id} url={article.url} />
-          ))} */}
+          ))}
         </div>
       </div>
     </div>
